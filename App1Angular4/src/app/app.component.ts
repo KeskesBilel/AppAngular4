b73cc4a4de967aa0773ed1f7e856b2c27d7e881b
KeskesBilel <bilel-keskes-2008>
2018-04-17 02:39:13 +0100

    installer et ecrire un application angular

---
+++ App1Angular4/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  /**
+   * ecrire un string
+   *  title et en afficher 
+   * avec {{}} en html 
+   **/
+  title = 'App';
+
+}
