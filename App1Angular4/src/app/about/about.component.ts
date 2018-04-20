d34006d5de57882a19660dbc868b07330f95c512
KeskesBilel <bilel-keskes-2008>
2018-04-20 01:24:39 +0100

    Angular Data Binding OutData
    affiche de component appabout a page root et afficher de donnee et et de
    tableau

--- App1Angular4/src/app/about/about.component.ts
+++ App1Angular4/src/app/about/about.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
-
+  info = {
+    nom: "bilel",
+    email:"bilelkeskes1@gmail.com",
+    tel : "02568"
+  
+} 
+comments =[
+    {
+      data :new Date(),message:"aaa"
+    },
+    {
+      data :new Date(),message:"aaa"
+ },
+    {
+      data :new Date(),message:"aaa"  }
+]
   constructor() { }
 
   ngOnInit() {
   }
+
 
 }
